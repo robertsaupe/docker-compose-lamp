c7249abdf9c533966b4c20427213aaaf394b5f7e
Robert Saupe <robert@mysaupe.de>
2021-09-26 20:32:19 +0200

    change local permissions on stop

--- stop.sh
+++ stop.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 cd "$(dirname "$(readlink -f "$0" || realpath "$0")")"
 
+if [ -f .env ]
+then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+fi
+
+act_user=$(id -u)
+
+docker exec -d $COMPOSE_PROJECT_NAME-$DATABASE chown -R $act_user /var/lib/mysql
+docker exec -d $COMPOSE_PROJECT_NAME-$DATABASE chown -R $act_user /var/log/mysql
+docker exec -d $COMPOSE_PROJECT_NAME-$PHPVERSION chown -R $act_user /var/www/html
+docker exec -d $COMPOSE_PROJECT_NAME-$PHPVERSION chown -R $act_user /var/log/apache2
+
 docker-compose down
 
 #read -p "Press any key to resume ..."
