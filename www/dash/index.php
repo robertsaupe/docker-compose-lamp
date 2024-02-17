d423659dd78e3573588a1cf6282f73a37aad6f3d
Robert Saupe <mail@robertsaupe.de>
2024-02-17 16:10:40 +0100

    added cmd for startup on windows with wsl added on index.php favicon and footer

--- www/dash/index.php
+++ www/dash/index.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>LAMP STACK</title>
+        <link rel="shortcut icon" href="/assets/images/favicon.svg" type="image/svg+xml">
         <link rel="stylesheet" href="/assets/css/bulma.min.css">
     </head>
     <body>
@@ -33,7 +34,7 @@
                                     <?php
                                     $link = mysqli_connect("database", "root", $_ENV['MYSQL_ROOT_PASSWORD'], null);
 
-/* check connection */
+                                    /* check connection */
                                     if (mysqli_connect_errno()) {
                                         printf("MySQL connecttion failed: %s", mysqli_connect_error());
                                     } else {
@@ -62,5 +63,17 @@
                 </div>
             </div>
         </section>
+        <footer class="footer">
+            <div class="content has-text-centered">
+                <p>
+                    <strong><a href="https://robertsaupe.de" target="_blank">Robert Saupe</a></strong><br>
+                    <br>
+                    <a href="https://github.com/robertsaupe/docker-compose-lamp" target="_blank">github.com/robertsaupe/docker-compose-lamp</a><br>
+                    The source code is released under the <a href="https://github.com/robertsaupe/docker-compose-lamp/blob/master/LICENSE" target="_blank">MIT license</a>.<br>
+                    <br>
+                    The project is a fork of <a href="https://github.com/sprintcube/docker-compose-lamp" target="_blank">github.com/sprintcube/docker-compose-lamp</a>.<br>
+                </p>
+            </div>
+        </footer>
     </body>
 </html>
